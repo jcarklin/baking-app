e4e541734be3baaa192c148840ead0f9a0ec85d4
joelc <joelc@ipay.co.za>
2018-10-18 17:49:26 +0200

    Initial commit - adding room

--- app/src/main/java/jcarklin/co/za/bakingrecipes/repository/db/BakingAppDao.java
+++ app/src/main/java/jcarklin/co/za/bakingrecipes/repository/db/BakingAppDao.java
@@ -6,21 +6,30 @@ import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
 
 import java.util.List;
 import java.util.Set;
 
 import jcarklin.co.za.bakingrecipes.repository.model.Ingredient;
 import jcarklin.co.za.bakingrecipes.repository.model.Recipe;
+import jcarklin.co.za.bakingrecipes.repository.model.RecipeComplete;
+import jcarklin.co.za.bakingrecipes.repository.model.Step;
 
 @Dao
 public interface BakingAppDao {
 
     @Query("SELECT * FROM recipes")
-    LiveData<List<Recipe>> fetchAllRecipes();
+    LiveData<List<RecipeComplete>> fetchAllRecipes();
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    long addRecipes(Recipe... recipes);
+    long addRecipe(Recipe recipe);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long addIngredients(Ingredient... ingredients);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    long addSteps(Step... steps);
 
     @Delete
     void clearRecipes();
@@ -28,4 +37,9 @@ public interface BakingAppDao {
     @Query("SELECT * FROM ingredients WHERE shopping_list = 1")
     Set<Ingredient> getShoppingList();
 
+    @Query("DELETE FROM ingredients WHERE shopping_list = 0")
+    Set<Ingredient> clearIngredients();
+
+    @Update
+    int updateShoppingList(Ingredient ingredient);
 }
--- app/src/main/java/jcarklin/co/za/bakingrecipes/repository/model/RecipeComplete.java
+++ app/src/main/java/jcarklin/co/za/bakingrecipes/repository/model/RecipeComplete.java
@@ -1,49 +1,65 @@
 package jcarklin.co.za.bakingrecipes.repository.model;
 
-import java.util.List;
-
-import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.PrimaryKey;
 import android.arch.persistence.room.Relation;
 import android.os.Parcel;
 import android.os.Parcelable;
-import android.os.Parcelable.Creator;
+
 import com.squareup.moshi.Json;
 
-@Entity(tableName = "recipes")
-public class Recipe implements Parcelable
+import java.util.List;
+
+public class RecipeComplete implements Parcelable
 {
 
     @PrimaryKey
+    @Json(name = "id")
     private Integer id;
 
+    @Json(name = "name")
     private String name;
 
+    @Relation(parentColumn = "id", entityColumn = "recipe_id")
+    @Json(name = "ingredients")
+    private List<Ingredient> ingredients = null;
+
+    @Relation(parentColumn = "id", entityColumn = "recipe_id")
+    @Json(name = "steps")
+    private List<Step> steps = null;
+
+    @Json(name = "servings")
     private Integer servings;
 
+    @Json(name = "image")
     private String image;
 
-    public Recipe() {
+    public RecipeComplete() {
     }
 
     /**
      *
+     * @param ingredients
      * @param id
      * @param servings
      * @param name
      * @param image
+     * @param steps
      */
-    public Recipe(Integer id, String name, Integer servings, String image) {
+    public RecipeComplete(Integer id, String name, List<Ingredient> ingredients, List<Step> steps, Integer servings, String image) {
         super();
         this.id = id;
         this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
         this.servings = servings;
         this.image = image;
     }
 
-    private Recipe(Parcel in) {
+    private RecipeComplete(Parcel in) {
         this.id = ((Integer) in.readValue((Integer.class.getClassLoader())));
         this.name = ((String) in.readValue((String.class.getClassLoader())));
+        in.readList(this.ingredients, (Ingredient.class.getClassLoader()));
+        in.readList(this.steps, (Step.class.getClassLoader()));
         this.servings = ((Integer) in.readValue((Integer.class.getClassLoader())));
         this.image = ((String) in.readValue((String.class.getClassLoader())));
     }
@@ -62,6 +78,22 @@ public class Recipe implements Parcelable
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
     }
 
     public Integer getServings() {
@@ -84,6 +116,8 @@ public class Recipe implements Parcelable
     public void writeToParcel(Parcel dest, int flags) {
         dest.writeValue(id);
         dest.writeValue(name);
+        dest.writeList(ingredients);
+        dest.writeList(steps);
         dest.writeValue(servings);
         dest.writeValue(image);
     }
@@ -93,14 +127,14 @@ public class Recipe implements Parcelable
         return  0;
     }
 
-    public final static Parcelable.Creator<Recipe> CREATOR = new Creator<Recipe>() {
+    public final static Creator<RecipeComplete> CREATOR = new Creator<RecipeComplete>() {
 
-        public Recipe createFromParcel(Parcel in) {
-            return new Recipe(in);
+        public RecipeComplete createFromParcel(Parcel in) {
+            return new RecipeComplete(in);
         }
 
-        public Recipe[] newArray(int size) {
-            return (new Recipe[size]);
+        public RecipeComplete[] newArray(int size) {
+            return (new RecipeComplete[size]);
         }
 
     };
