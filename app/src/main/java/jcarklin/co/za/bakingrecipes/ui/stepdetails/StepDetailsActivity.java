de96608560d3911eaef156d4ada22a3f6f005346
joelc <joelc@ipay.co.za>
2018-11-12 16:19:21 +0200

    reorganised

---
+++ app/src/main/java/jcarklin/co/za/bakingrecipes/ui/stepdetails/StepDetailsActivity.java
@@ -0,0 +1,15 @@
+package jcarklin.co.za.bakingrecipes.ui.stepdetails;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jcarklin.co.za.bakingrecipes.R;
+
+public class StepDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_step_details);
+    }
+}
